8328c01dbd5d4d52cc8a807528ec942ec3fe4ffe
David <drbotro@gmail.com>
2017-12-08 12:11:04 +0100

    Firebase filtrar datos

--- 09-firebase/11-firebase-filtrar-resultados/js/main.js
+++ 09-firebase/11-firebase-filtrar-resultados/js/main.js
@@ -1,54 +1,25 @@
 const gatosRef = db.ref('gatos');
-const gatosVotosRef = db.ref('votos');
-
-// Datos iniciales
-const gatos = [
-    {nombre: 'Rouco', edad: 6, color: 'blanco y negro'},
-    {nombre: 'Mona', edad: 3, color: 'atrigrado'},
-    {nombre: 'Mino', edad: 1, color: 'negro'},
-    {nombre: 'Guizno', color: 'negro'},
-];
-
 
 new Vue({
     el: '#app',
     created(){
-        gatos.forEach(gato => {
-            gatosRef.child(gato.nombre).set({
-                edad: gato.edad || null,
-                color: gato.color
-            });
-        });
-
-        gatos.forEach(gato => {
-            gatosVotosRef.child(gato.nombre).set(
-               Math.floor(Math.random() * 1000) + 0
-            )
-        });
-
-        this.gatos.orderByChild('edad').on('child_moved', snapshot => {
-            console.warn(snapshot.key, snapshot.val());
-        });
-
+        this.ordenarPorKeySecundaria('edad');
     },
     data:{
         gatos: gatosRef,
     },
     methods :{
         ordenarPorKeySecundaria(key){
-            this.gatos.orderByChild(key).on('child_added', snapshot => {
+            this.gatos
+                .orderByChild(key)
+                //.limitToFirst(2)
+                //.limitToLast(1)
+                //.startAt(5)
+                //.endAt(6)
+                .equalTo(3)
+                .on('child_added', snapshot => {
                 console.log(snapshot.key, snapshot.val());
-            })
-        },
-        ordenarPorKey(){
-            this.gatos.orderByKey().on('child_added', snapshot => {
-                console.log(snapshot.key, snapshot.val());
-            })
-        },
-        ordenarPorValor(){
-            gatosVotosRef.orderByValue().on('child_added', snapshot => {
-                console.log(snapshot.key, snapshot.val());
-            })
+            });
         },
 
     }
